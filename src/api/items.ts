f7f9a71f8bc3b3b3d2c164f2b59b82b8cf7bdad5
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-06-16 18:29:02 +0800

    [Misc] Modified routes to adhere to REST standards

--- src/api/items.ts
+++ src/api/items.ts
@@ -2,11 +2,11 @@ import type { CreateItemDTO } from '../types/item';
 import api from "./axios";
 
 export async function fetchItems() {
-  const res = await api.get("/Items");
+  const res = await api.get("/items");
   return res.data;
 }
 
 export async function createItem(data: CreateItemDTO) {
-  const res = await api.post("/Items", data);
+  const res = await api.post("/items", data);
   return res.data;
 }
