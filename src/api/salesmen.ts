2ec40e6d6f239988ac8dcbeed11f9f39313e4fe6
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-06-16 23:31:29 +0800

    [Fix] Fixed salesmen route link

--- src/api/salesmen.ts
+++ src/api/salesmen.ts
@@ -2,21 +2,21 @@ import type { CreateSalesmanDTO, Salesman } from '../types/salesman';
 import api from "./axios";
 
 export async function fetchSalesmen(): Promise<Salesman[]> {
-  const res = await api.get("/salesmens");
+  const res = await api.get("/salesmen");
   return res.data;
 }
 
 export async function createSalesman(data: CreateSalesmanDTO) {
-  const res = await api.post("/salesmens", data);
+  const res = await api.post("/salesmen", data);
   return res.data;
 }
 
 export async function editSalesman(id: number, data: CreateSalesmanDTO) {
-  const res = await api.put(`/salesmens/${id}`, data);
+  const res = await api.put(`/salesmen/${id}`, data);
   return res.data;
 }
 
 export async function deleteSalesman(id: number) {
-  const res = await api.delete(`/salesmens/${id}`);
+  const res = await api.delete(`/salesmen/${id}`);
   return res.data;
 }
